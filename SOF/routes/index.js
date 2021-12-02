6056e9f1e8f3fa0556ef82eabcbd32d5cb95346c
zirpoli <tizirpoli@criasol.com.br>
2021-12-02 18:52:43 -0300

    ajuste rota home e barra

--- SOF/routes/index.js
+++ SOF/routes/index.js
@@ -2,59 +2,59 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('home', { title: 'Home page' });
 });
 
-router.get('/home', function(req, res){
-  res.render('home', {title: 'Home page'});
+router.get('/home', function (req, res) {
+  res.render('home', { title: 'Home page' });
 });
 
-router.get('/about', function(req, res){
-  res.render('about', {title: 'Sobre Nós'})
+router.get('/about', function (req, res) {
+  res.render('about', { title: 'Sobre Nós' })
 })
 
-router.get('/contact', function(req, res){
-  res.render('contact', {title: 'Contato'})
+router.get('/contact', function (req, res) {
+  res.render('contact', { title: 'Contato' })
 })
 
-router.get('/credits', function(req, res){
-  res.render('credits', {title: 'Créditos'})
+router.get('/credits', function (req, res) {
+  res.render('credits', { title: 'Créditos' })
 })
 
-router.get('/ranking', function(req, res){
-  res.render('ranking', {title: 'Ranking'})
+router.get('/ranking', function (req, res) {
+  res.render('ranking', { title: 'Ranking' })
 })
 
-router.get('/search', function(req, res){
-  res.render('search', {title: 'Busca'})
+router.get('/search', function (req, res) {
+  res.render('search', { title: 'Busca' })
 })
 
-router.get('/dashboard', function(req, res){
-  res.render('dashboard', {title: 'Painel de Controle'})
+router.get('/dashboard', function (req, res) {
+  res.render('dashboard', { title: 'Painel de Controle' })
 })
 
-router.get('/newthread', function(req, res){
-  res.render('newthread', {title: 'Nvo Fio'})
+router.get('/newthread', function (req, res) {
+  res.render('newthread', { title: 'Nvo Fio' })
 })
 
-router.get('/threadsmgmt', function(req, res){
-  res.render('threadsmgmt', {title: 'Gerencie Seus Fios'})
+router.get('/threadsmgmt', function (req, res) {
+  res.render('threadsmgmt', { title: 'Gerencie Seus Fios' })
 })
 
-router.get('/profileview', function(req, res){
-  res.render('profileview', {title: 'Perfil de @Usuário | Segue o Fio'})
+router.get('/profileview', function (req, res) {
+  res.render('profileview', { title: 'Perfil de @Usuário | Segue o Fio' })
 })
 
-router.get('/accountmgmt', function(req, res){
-  res.render('accountmgmt', {title: 'Edite Seu Perfil'})
+router.get('/accountmgmt', function (req, res) {
+  res.render('accountmgmt', { title: 'Edite Seu Perfil' })
 })
 
-router.get('/login', function(req, res){
-  res.render('login', {title: 'Login'})
+router.get('/login', function (req, res) {
+  res.render('login', { title: 'Login' })
 })
 
-router.post('/login', function(req, res){
+router.post('/login', function (req, res) {
   let user = {
     username: req.body.username,
     password: req.body.paswword,
