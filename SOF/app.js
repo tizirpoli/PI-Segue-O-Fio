b3cb7ee8f6ce97b58cecdedb5f7cb0262da90c0a
Felipe Araujo <felipearaujo@MacBook-Pro-de-Felipe.local>
2021-12-09 13:21:49 -0300

    Problema do avatar resolvido, adicionado app.use() app.js e modificado o ejs headerLogin

--- SOF/app.js
+++ SOF/app.js
@@ -15,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static('public/uploads'));
 
 app.use(session({
   secret: "projetoIntegradorBoard",
